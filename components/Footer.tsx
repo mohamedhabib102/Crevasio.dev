a392e885aacdd5453b4757938f78836cd3031de5
Mohamed Habib <byaalkhy43@gmail.com>
2025-12-11 16:23:09 +0200

    add animation of sections

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,11 +7,11 @@ import ScrollRevealBox from "@/ui/ScrollRevealBox";
 const Footer = () => {
     const { t } = useLanguage();
     return (
-        <footer className="bg-white dark:bg-gray-900 text-gray-600 dark:text-gray-300 py-12">
+        <footer className="bg-gray-200 dark:bg-gray-900 text-gray-600 dark:text-gray-300 py-12">
             <CustomContainer>
                 <ScrollRevealBox>
                     <div className="flex flex-col items-center w-full">
-                        <div className="flex flex-col md:flex-row-reverse items-center justify-between w-full gap-8 pb-8 mb-3 border-b border-gray-200 dark:border-gray-800">
+                        <div className="flex flex-col md:flex-row-reverse items-center justify-between w-full gap-8 pb-8 mb-3 border-b border-gray-300 dark:border-gray-800">
                             <Logo className="footer-logo" />
                             <p className="text-gray-500 dark:text-gray-400 max-w-lg text-center md:text-right">
                                 {t.footer.description}
